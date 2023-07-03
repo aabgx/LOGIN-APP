30dbecb414cc96ff26757f71600381d037df7eeb
Andreea Bugnar <116505275+aabgx@users.noreply.github.com>
2023-07-03 16:10:25 +0300

    commit initial

---
+++ server/login_app/src/test/java/com/example/login_app/LoginAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.login_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
