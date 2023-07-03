d2a5d8f44523962bc78e1a128a401b08219bcb46
Andreea Bugnar <116505275+aabgx@users.noreply.github.com>
2023-07-03 15:47:10 +0300

    commit 2

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const sir = ["yellow", "white", "red", "blue", "green"];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        {sir.map((color) => {
+          return (
+            <div
+              style={{ backgroundColor: color }}
+              className="myDiv"
+              key={color}
+            ></div>
+          );
+        })}
+      </div>
+      <div></div>
+    </>
   );
 }
 
