344340dd37a4e731c0bfa532fbe35d8da79a473c
Andreea Bugnar <116505275+aabgx@users.noreply.github.com>
2023-07-17 10:39:48 +0300

    beginning of exception handling and security

--- server/login_app/src/main/java/com/example/login_app/repository/UserRepository.java
+++ server/login_app/src/main/java/com/example/login_app/repository/UserRepository.java
@@ -9,6 +9,5 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User,Integer> {
     Optional<User> findByEmail(String email);
-
     void deleteById(Integer id);
 }
