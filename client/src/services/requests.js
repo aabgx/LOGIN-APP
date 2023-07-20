61a99c8b5c247a4fc1255db59e8c83ae0ba865b2
Andreea Bugnar <116505275+aabgx@users.noreply.github.com>
2023-07-20 11:15:14 +0300

    missing file

--- client/src/services/requests.js
+++ client/src/services/requests.js
@@ -3,36 +3,25 @@ import axiosInstance from "./axios.js";
 export const putRequest = async function (email, userDetails) {
   try {
     const response = await axiosInstance.put(`/users/${email}`, userDetails);
-    // console.log(response);
-  } catch (err) {
-    // console.log(err);
-  }
+  } catch (err) {}
 };
 
 export const getRequest = async function () {
   try {
     const response = await axiosInstance.get("/users");
     return response;
-  } catch (err) {
-    // console.log(err);
-  }
+  } catch (err) {}
 };
 
 export const deleteRequest = async function (email) {
   try {
     const response = await axiosInstance.delete(`/users/${email}`);
-    // console.log(response);
-  } catch (err) {
-    // console.log(err);
-  }
+  } catch (err) {}
 };
 
 export const postRequest = async function (userDetails) {
   try {
     const response = await axiosInstance.post("/users", userDetails);
-    // console.log(response);
     return response;
-  } catch (err) {
-    // console.log(err);
-  }
+  } catch (err) {}
 };
